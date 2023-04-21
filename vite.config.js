2e5382e0cd0abf6f590ef65d1189fc8353364723
StevenNizamaMartinez <0331172014@unjfsc.edu.pe>
2023-04-21 15:08:22 -0500

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base : "https://StevenNizamaMartinez.github.io/krowdi-react-prueba"
+  plugins: [react()]
 })
